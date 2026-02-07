361a054b9c2e78d6ab8ec6c22e494d27b7854a07
Brandon Jaus <brandon.jaus@gmail.com>
2026-02-07 16:32:52 -0600

    chore: add SECURITY.md and benchmarks
    Add security policy using GitHub's private vulnerability reporting.
    Add benchmarks for Do() and backoff strategies.

---
+++ benchmark_test.go
@@ -0,0 +1,86 @@
+package retry
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+)
+
+type immediateClock struct{}
+
+func (immediateClock) Now() time.Time                             { return time.Now() }
+func (immediateClock) Sleep(context.Context, time.Duration) error { return nil }
+
+func BenchmarkDo_ImmediateSuccess(b *testing.B) {
+	ctx := context.Background()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Do(ctx, func(ctx context.Context) error {
+			return nil
+		}, WithClock(immediateClock{}))
+	}
+}
+
+func BenchmarkDo_OneRetry(b *testing.B) {
+	ctx := context.Background()
+	errTest := errors.New("test")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		attempt := 0
+		Do(ctx, func(ctx context.Context) error {
+			attempt++
+			if attempt < 2 {
+				return errTest
+			}
+			return nil
+		}, WithClock(immediateClock{}))
+	}
+}
+
+func BenchmarkDo_Exhausted(b *testing.B) {
+	ctx := context.Background()
+	errTest := errors.New("test")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Do(ctx, func(ctx context.Context) error {
+			return errTest
+		},
+			WithMaxAttempts(3),
+			WithClock(immediateClock{}),
+		)
+	}
+}
+
+func BenchmarkPolicy_Do(b *testing.B) {
+	ctx := context.Background()
+	policy := New(WithMaxAttempts(3), WithClock(immediateClock{}))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		policy.Do(ctx, func(ctx context.Context) error {
+			return nil
+		})
+	}
+}
+
+func BenchmarkBackoff_Exponential(b *testing.B) {
+	backoff := Exponential(100 * time.Millisecond)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		backoff.Delay(i % 10)
+	}
+}
+
+func BenchmarkBackoff_ExponentialWithJitter(b *testing.B) {
+	backoff := WithJitter(0.2, Exponential(100*time.Millisecond))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		backoff.Delay(i % 10)
+	}
+}
